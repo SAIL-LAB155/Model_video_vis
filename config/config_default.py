001f4e041b80b2539d1899c258c38e1fd0331100
cheungbh <534660436@qq.com>
2020-10-27 10:44:28 +0800

    config

--- config/config_default.py
+++ config/config_default.py
@@ -1,9 +1,7 @@
 import torch
 from config.opt import opt
 
-
 img_folder = "img/test"
-frame_size = (opt.video_width, opt.video_height)
 
 '''
 ------------------------------------------------------------------------------------------
@@ -15,11 +13,6 @@ write_kps = False
 device = "cuda:0"
 print("Using {}".format(device))
 
-confidence = 0.05
-num_classes = 80
-nms_thresh = 0.33
-input_size = 416
-
 track_idx = "all"    # If all idx, track_idx = "all"
 track_plot_id = ["all"]   # If all idx, track_plot_id = ["all"]
 assert track_idx == "all" or isinstance(track_idx, int)
@@ -27,3 +20,8 @@ assert track_idx == "all" or isinstance(track_idx, int)
 plot_bbox = True
 plot_kps = True
 plot_id = True
+
+resize_ratio = 0.5
+show_size = (1280, 480)
+store_size = (1280, 480)
+
